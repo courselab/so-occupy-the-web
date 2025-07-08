ff72f820b95fe82cbc76deb3f5714c68d4f10e1c
Marrafon, V. M. <marrafonmv.dev@gmail.com>
2025-07-08 18:51:54 -0300

    Delete timec.h

--- timec.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIMEC_H
-#define TIMEC_H
-
-int  __attribute__((fastcall, naked)) readtime (void);
-
-// Receive a time in Binary Decimal Coded
-void __attribute__((fastcall, naked)) settime  (unsigned short);
-
-#endif /*TIMEC_H*/
